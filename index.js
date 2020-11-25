11544f2b7e5991be5feefd0e5d5181cd8e3e33bb
Hung Vu <hunghvu2017@gmail.com>
2020-11-25 10:52:58 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * @author Hung Vu
+ */
+const express = require('express')
+const app = express()
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
+
+var path = require('path')
+
+//A Pool of DB connections. 
+const { Pool } = require('pg')
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+})
+
+app.use("/", express.static('pages'))
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server up and running on port: " + (process.env.PORT || 5000));
+})
