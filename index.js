f6b6ae5c8ff73d96ac5fab7fc4fa6dbd574888fe
Hung Vu <hunghvu2017@gmail.com>
2020-11-28 21:18:44 -0800

    Set up a barebone homepage

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  * 
  * @author Hung Vu
  */
+
+ // Set up required element for index.jsjs
 const express = require('express')
 const app = express()
 
@@ -10,22 +12,33 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 
 var path = require('path')
+
+// Using EJS for dynamic front end.
 app.set('view engine', 'ejs')
 app.engine('ejs', require('ejs').__express)
 app.set('views', path.join(__dirname, "views"))
+
+const favicon = require('serve-favicon')
+
+// Serve static files.
+app.use(express.static('public'))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // app.use("/", express.static('pages'))
 
 // For local testing and development.
 // app.use("/", express.static('demo-site'))
 // app.use("/search", require('./demo-routes/home_demo.js'))
+// app.use("/search", require('./demo-routes/home_demo.js'))
+
 app.get("/", (request, response) => {
     response.render('index', {})
 })
-app.get("/project", (request, response) => {
-    response.render('project')
-})
-app.use("/search", require('./demo-routes/home_demo.js'))
+// Not implement.
+// app.get("/project", (request, response) => {
+//     response.render('project')
+// })
+
 
 
 app.listen(process.env.PORT || 5000, () => {
