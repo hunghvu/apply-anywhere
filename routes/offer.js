3db37cac84daa72cb68656614cc8d2cddaba2bbc
Hung Vu <hunghvu2017@gmail.com>
2020-12-03 10:16:35 -0800

    Fix injection vulnerability

--- routes/offer.js
+++ routes/offer.js
@@ -1,7 +1,4 @@
-/**
- * This node provide Offer page with dynamic drop down menu.
- * @author Hung Vu
- */
+const { Console } = require('console')
 const express = require('express')
 const app = express()
 
@@ -13,27 +10,46 @@ var path = require('path')
 app.set('view engine', 'ejs')
 app.engine('ejs', require('ejs').__express)
 app.set('views', path.join(__dirname, "views"))
-
-router.get("/", (request, response) => {
+var job_title
+router.get("/", (request, response, next) => {
     // const value = request.query.last_name
     // vulnerable to sql injection?
     const theQuery = "SELECT JOB_TITLE FROM JOBS;" 
     // console.log(value)
-
     pool.query(theQuery)
         .then(result => {
-            // console.log(result.rows[0].job_title),
+            job_title = result.rows
+        })
+    next()
+}, (request, response) => {
+    // const value = request.query.job_title
+    // console.log(value)
+    // vulnerable to sql injection?
+    // TODO Separate to query + value to avoid injection, can combine offer and serachoffer.js
+    // const theQuery = " SELECT JOBS.JOB_TITLE, JOBS.EXP_SAL, CAREERS.INDUSTRY, LISTINGS.COMPANY "
+    //                     + "FROM   JOBS "
+    //                     + "JOIN   CAREERS "
+    //                     + "ON     JOBS.JOB_TITLE = CAREERS.JOB_TITLE " 
+    //                     + "JOIN   LISTINGS "
+    //                     + "ON     JOBS.JOB_TITLE = LISTINGS.JOB_TITLE "
+    //                     + "WHERE  JOBS.JOB_TITLE = '" + value + "';"
 
-            console.log(result)
-            // response.send({
-            //     job_title: result.job_title
-            // }),
-            // response.sendFile(path.join(__dirname, "../demo-site/project.html"))
-            response.render('offer', {
-                job_title: result.rows
-            })
-        }
-        )
-})
+    value = [request.query.job_title]
+    const theQuery = " SELECT JOBS.JOB_TITLE, JOBS.EXP_SAL, CAREERS.INDUSTRY, LISTINGS.COMPANY "
+                            + "FROM   JOBS "
+                            + "JOIN   CAREERS "
+                            + "ON     JOBS.JOB_TITLE = CAREERS.JOB_TITLE " 
+                            + "JOIN   LISTINGS "
+                            + "ON     JOBS.JOB_TITLE = LISTINGS.JOB_TITLE "
+                            + "WHERE  JOBS.JOB_TITLE = $1;"
+    pool.query(theQuery, value).then(result => {
+        console.log(result)
+        response.render('offer', {
+            job_title : job_title,
+            result : result.rows
+        })
+    })
+}
+)
 
 module.exports = router
