e79f60f15facf9a837dcbd1d0b49f457e3e88afe
Hung Vu <hunghvu2017@gmail.com>
2020-12-03 00:26:43 -0800

    Dynamically populate drop down offer menu

--- index.js
+++ index.js
@@ -5,52 +5,50 @@
  */
 
  // Set up required element for index.jsjs
-const express = require('express')
+const express = require("express")
 const app = express()
 
 const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 
-var path = require('path')
+var path = require("path")
 
 // Using EJS for dynamic front end.
-app.set('view engine', 'ejs')
-app.engine('ejs', require('ejs').__express)
-app.set('views', path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.engine("ejs", require("ejs").__express)
+app.set("views", path.join(__dirname, "views"))
 
-const favicon = require('serve-favicon')
+const favicon = require("serve-favicon")
 
 // Serve static files.
-app.use(express.static('public'))
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(express.static("public"))
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
 
-// app.use("/", express.static('pages'))
+// app.use("/", express.static("pages"))
 
 // For local testing and development.
-// app.use("/", express.static('demo-site'))
-// app.use("/search", require('./demo-routes/home_demo.js'))
-// app.use("/search", require('./demo-routes/home_demo.js'))
+// app.use("/", express.static("demo-site"))
+// app.use("/search", require("./demo-routes/home_demo.js"))
+// app.use("/search", require("./demo-routes/home_demo.js"))
 
 // End point for home page, offer page, career path, salary (city) and salary (company).
 app.get("/", (request, response) => {
-    response.render('index', {})
+    response.render("index", {})
 })
 
-app.get("/offer", (request, response) => {
-    response.render('offer', {})
-})
+app.use("/offer", require("./routes/offer.js"))
 app.get("/career", (request, response) => {
-    response.render('salcity', {})
+    response.render("salcity", {})
 })
 app.get("/salarypercity", (request, response) => {
-    response.render('salcity', {})
+    response.render("salcity", {})
 })
 app.get("/salarypercompany", (request, response) => {
-    response.render('salcompany', {})
+    response.render("salcompany", {})
 })
 // Not implement.
 // app.get("/project", (request, response) => {
-//     response.render('project')
+//     response.render("project")
 // })
 
 app.listen(process.env.PORT || 5000, () => {
---
+++ routes/offer.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+
+var router = express.Router()
+
+let pool = require('../utilities/utils').pool
+
+var path = require('path')
+app.set('view engine', 'ejs')
+app.engine('ejs', require('ejs').__express)
+app.set('views', path.join(__dirname, "views"))
+
+router.get("/", (request, response) => {
+    // const value = request.query.last_name
+    // vulnerable to sql injection?
+    const theQuery = "SELECT JOB_TITLE FROM JOBS;" 
+    // console.log(value)
+
+    pool.query(theQuery)
+        .then(result => {
+            // console.log(result.rows[0].job_title),
+
+            console.log(result)
+            // response.send({
+            //     job_title: result.job_title
+            // }),
+            // response.sendFile(path.join(__dirname, "../demo-site/project.html"))
+            response.render('offer', {
+                job_title: result.rows
+            })
+        }
+        )
+})
+
+module.exports = router
