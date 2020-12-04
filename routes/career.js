d8452c5c45972a24353347ee2ea7701cacfdabee
Hung Vu <hunghvu2017@gmail.com>
2020-12-03 18:21:20 -0800

    Set up groundwork for career page

--- routes/career.js
+++ routes/career.js
@@ -3,49 +3,50 @@
  * @author Hung vu
  */
 
- // Setup
- const { Console } = require('console')
- const express = require('express')
- const app = express()
- 
- var router = express.Router()
- 
- let pool = require('../utilities/utils').pool
- 
- var path = require('path')
- app.set('view engine', 'ejs')
- app.engine('ejs', require('ejs').__express)
- app.set('views', path.join(__dirname, "views"))
- 
- var job_title
- router.get("/", (request, response, next) => {
-     response.render('career', {})
-     // Info for drop down menu
-//      const theQuery = "SELECT JOB_TITLE FROM JOBS;" 
-//      pool.query(theQuery)
-//          .then(result => {
-//              job_title = result.rows
-//          })
-//      next()
-//  }, (request, response) => {
-//      // Info to populate table
-//      value = [request.query.job_title]
-//      const theQuery = " SELECT JOBS.JOB_TITLE, JOBS.EXP_SAL, CAREERS.INDUSTRY, LISTINGS.COMPANY "
-//                              + "FROM   JOBS "
-//                              + "JOIN   CAREERS "
-//                              + "ON     JOBS.JOB_TITLE = CAREERS.JOB_TITLE " 
-//                              + "JOIN   LISTINGS "
-//                              + "ON     JOBS.JOB_TITLE = LISTINGS.JOB_TITLE "
-//                              + "WHERE  JOBS.JOB_TITLE = $1;"
-//      pool.query(theQuery, value).then(result => {
-//          console.log(result)
-//          response.render('career', {
-//              job_title : job_title,
-//              result : result.rows
-//          })
-//      })
- }
- )
- 
- module.exports = router
- 
+// Setup
+const { Console } = require('console')
+const express = require('express')
+const app = express()
+
+var router = express.Router()
+
+let pool = require('../utilities/utils').pool
+
+var path = require('path')
+app.set('view engine', 'ejs')
+app.engine('ejs', require('ejs').__express)
+app.set('views', path.join(__dirname, "views"))
+
+var job_title
+router.get("/", (request, response, next) => {
+    // response.render('career', {})
+    // Info for drop down menu
+    const theQuery = "SELECT JOB_TITLE FROM JOBS;"
+    pool.query(theQuery)
+        .then(result => {
+            job_title = result.rows
+        })
+    next()
+}, (request, response) => {
+    // Info to populate table
+    value = [request.query.job_title]
+    const theQuery =  "SELECT     DERIVED_TABLE.sum, DERIVED_TABLE.JOB_TITLE, CAREERS.INDUSTRY "
+                    + "FROM       CAREERS "
+                    + "FULL JOIN (    SELECT SUM(LISTINGS.JOB_OPENINGS), LISTINGS.JOB_TITLE "
+                                    + "FROM LISTINGS "
+                                    + "GROUP BY LISTINGS.JOB_TITLE) AS DERIVED_TABLE "
+           
+                    + "ON         DERIVED_TABLE.JOB_TITLE = CAREERS.JOB_TITLE "
+                    // + "WHERE      DERIVED_TABLE.JOB_TITLE = $1 "
+                    + "ORDER BY   DERIVED_TABLE.SUM DESC; "
+    pool.query(theQuery).then(result => {
+        console.log(result)
+        response.render('career', {
+            job_title: job_title,
+            result: result.rows
+        })
+    })
+}
+)
+
+module.exports = router
